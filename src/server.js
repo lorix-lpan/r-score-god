169aaef2882cbad96f96de941c68f082c7b2efd8
Lawrence Pan <lawrencefeipan@gmail.com>
2016-04-16 00:31:17 -0400

    add example code from fb to test

--- src/server.js
+++ src/server.js
@@ -7,6 +7,40 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+const token = "<page_access_token>";
+
+function sendTextMessage(sender, text) {
+  const messageData = { text };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    },
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+app.post('/webhook/', (req, res) => {
+  const messaging_events = req.body.entry[0].messaging;
+  for (let i = 0; i < messaging_events.length; i++) {
+    const event = req.body.entry[0].messaging[i];
+    const sender = event.sender.id;
+    if (event.message && event.message.text) {
+      const text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.get('/webhook/', (req, res) => {
   if (req.query['hub.verify_token'] === 'rob_the_r_score_god') {
     res.send(req.query['hub.challenge']);
