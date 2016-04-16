aeb7fdf3864b5c191e6c67e7e0a8b7390173d5ca
Lawrence Pan <lawrencefeipan@gmail.com>
2016-04-15 20:52:15 -0400

    add webhook route

--- src/server.js
+++ src/server.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+app.get('/webhook/', (req, res) => {
+  if (req.query['hub.verify_token'] === 'rob_the_r_score_god') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
 app.listen(serverPort, err => {
   if (!err) {
     console.log(`Server running on port ${serverPort}`);
