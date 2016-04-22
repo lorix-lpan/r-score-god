4e4a32f0a8dcbbe0136b1210e65ae89c75203d03
Matthew <scom8642@gmail.com>
2016-04-22 17:01:31 -0400

    fix naming errorx

--- src/response.js
+++ src/response.js
@@ -2,15 +2,15 @@ const token = 'CAANhtjyR1X8BANoATlQ1E4svnZBZCyZAe58vIKVZBSKqKJSIZAOwakwwjkEZA0ZA
 
 const NO_ANSWER = 3877009;
 let step = 0;
-let r-score;
+let rScore;
 // produce a response according to message from user
 function smartResponse(message, step) {
   switch(step){
-    case 0: return "what is your r-score, you little prick?";
+    case 0: return 'what is your r-score, you little prick?';
             step ++;
             break;
-    case 1: r-score =  checkForNumber(message);
-            if (r-score = NO_ANSWER){
+    case 1: rScore =  checkForNumber(message);
+            if (rScore = NO_ANSWER){
             return "Okay, what you said is very nice, but cut to the chase. What is your R-score? Answer!!!"
            break;
             }
