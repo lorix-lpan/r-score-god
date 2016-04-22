991ee53f5a0d8ba74efed63eff448fc415526df4
Matthew <scom8642@gmail.com>
2016-04-22 16:52:08 -0400

    strating with convincement

--- src/response.js
+++ src/response.js
@@ -1,8 +1,34 @@
 const token = 'CAANhtjyR1X8BANoATlQ1E4svnZBZCyZAe58vIKVZBSKqKJSIZAOwakwwjkEZA0ZAcUQzJgk5ZBLvwfzM4YG0xBA8DXzy9KSO7i0qetEBPhYiWR5Hgcf4rloBLRDaHNDeZAcXZAFCvUaei8DycZBuHV0j5Hfe7P2tsEut51EhIFP326GZBGZB9OgnmFO1jLn8G9JRqld4ZD'; // eslint-disable-line
 
+const NO_ANSWER = 3877009;
+let step = 0;
+let r-score;
 // produce a response according to message from user
-function smartResponse(message) {
+function smartResponse(message, step) {
+  switch(step){
+    case 0: return "what is your r-score, you little prick?";
+            step ++;
+            break;
+    case 1: r-score =  checkForNumber(message);
+            if (r-score = NO_ANSWER){
+            return "Okay, what you said is very nice, but cut to the chase. What is your R-score? Answer!!!"
+           break;
+            }
+            else{
+                                
+
+            }
+
+
+
+  }
+  return `${message} fuck you`;
+
 }
+
+
+
+
 
 function formatResponse({ text }, sender) {
   return {
@@ -12,7 +38,7 @@ function formatResponse({ text }, sender) {
     json: {
       recipient: { id: sender },
       message: {
-        text: text + 'lmao',
+        text: smartResponse(text),
       },
     },
   };
