6f4e28bf6874859afe31800eb36d7993c2293f71
Lawrence Pan <lawrencefeipan@gmail.com>
2016-04-23 00:10:28 -0400

    User model now can keep track of R Score, make the text less vulgar

--- src/actions/root.js
+++ src/actions/root.js
@@ -3,19 +3,22 @@ import users from '../users/index';
 
 // produce a response according to message from user
 function rootResponse(message, sender) {
+  // get reference of user on the user list
   const current = users.getUser(sender);
 
   switch (current.step) {
     case 0:
       current.addStep();
-      return 'what is your r-score, you little prick?';
+      return 'My sweet child, what is your lovely R Score?';
 
     case 1:
       const rScore = checkForNumber(message);
-      if (!rScore) {
-        return 'I dont care, cut to the chase. What is your R-score? Answer!!!';
+      if (rScore) {
+        current.setRscore(rScore);
+        return `I am sure ${rScore} R Score can get you a decent job.
+          Do you like flipping burgers?`;
       }
-      return `I now have your r-score, you have only ${rScore}.`;
+      return 'Just tell me your R Score you pleb.';
 
     default:
       return '';
